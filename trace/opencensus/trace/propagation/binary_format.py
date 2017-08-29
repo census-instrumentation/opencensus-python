d72720006b13f72f90f711fc1df69c5838254765
Angela Li <yanhuil@google.com>
2017-08-28 17:31:54 -0700

    WIP

--- trace/opencensus/trace/propagation/binary_format.py
+++ trace/opencensus/trace/propagation/binary_format.py
@@ -12,13 +12,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import binascii
+import collections
+import logging
 import struct
-
-from collections import namedtuple
 
 from opencensus.trace.span_context import SpanContext
 
 _ENABLED_BITMASK = 1
+
+BIG_ENDIAN = '>'
 
 UTF8 = 'utf-8'
 
@@ -28,31 +31,35 @@ SPAN_ID_FIELD_ID = 1
 TRACE_OPTION_FIELD_ID = 2
 
 ID_SIZE = 1
-TRACE_ID_SIZE = 32
-SPAN_ID_SIZE = 16
+TRACE_ID_SIZE = 16
+SPAN_ID_SIZE = 8
 TRACE_OPTION_SIZE = 1
 
 FORMAT_LENGTH = 4 * ID_SIZE + TRACE_ID_SIZE + SPAN_ID_SIZE + TRACE_OPTION_SIZE
 
 # See: https://docs.python.org/3/library/struct.html#format-characters
-INT_FORMAT = 'i'
 CHAR_ARRAY_FORMAT = 's'
+UNSIGNED_CHAR = 'B'
+UNSIGNED_LONG_LONG = 'Q'
 
-BINARY_FORMAT = '{version_id}' \
+# Adding big endian indicator add the beginning to avoid auto padding. This is
+# for ensuring the length of binary is not changed when propagating.
+BINARY_FORMAT = '{big_endian}{version_id}' \
     '{trace_id_field_id}{trace_id}' \
     '{span_id_field_id}{span_id}' \
     '{trace_option_field_id}{trace_option}'\
         .format(
-            version_id='{}{}'.format(ID_SIZE, INT_FORMAT),
-            trace_id_field_id='{}{}'.format(ID_SIZE, INT_FORMAT),
+            big_endian=BIG_ENDIAN,
+            version_id='{}'.format(UNSIGNED_CHAR),
+            trace_id_field_id='{}'.format(UNSIGNED_CHAR),
             trace_id='{}{}'.format(TRACE_ID_SIZE, CHAR_ARRAY_FORMAT),
-            span_id_field_id='{}{}'.format(ID_SIZE, INT_FORMAT),
-            span_id='{}{}'.format(SPAN_ID_SIZE, CHAR_ARRAY_FORMAT),
-            trace_option_field_id='{}{}'.format(ID_SIZE, INT_FORMAT),
-            trace_option='{}{}'.format(TRACE_OPTION_SIZE, CHAR_ARRAY_FORMAT)
+            span_id_field_id='{}'.format(UNSIGNED_CHAR),
+            span_id='{}'.format(UNSIGNED_LONG_LONG),
+            trace_option_field_id='{}'.format(UNSIGNED_CHAR),
+            trace_option='{}'.format(UNSIGNED_CHAR)
         )
 
-Header = namedtuple(
+Header = collections.namedtuple(
     'Header',
     'version_id '
     'trace_id_field_id '
@@ -82,13 +89,27 @@ class BinaryFormatPropagator(object):
         :rtype: :class:`~opencensus.trace.span_context.SpanContext`
         :returns: SpanContext generated from the trace context header.
         """
+        # First check the length of the binary, if cannot parse, return a new
+        # SpanContext and discard the context from binary.
+        if len(binary) != FORMAT_LENGTH:
+            logging.warn('Cannot parse the incoming binary data {}, '
+                         'wrong format.'.format(binary))
+            return SpanContext(from_header=False)
 
-        data = Header._make(struct.unpack(BINARY_FORMAT, binary))
-        trace_id = data.trace_id.decode(UTF8)
-        span_id = data.span_id.decode(UTF8)
-        trace_option = data.trace_option.decode(UTF8)
+        try:
+            data = Header._make(struct.unpack(BINARY_FORMAT, binary))
+        except struct.error:
+            logging.warn('Cannot parse the incoming binary data {}, '
+                         'wrong format.'.format(binary))
+            return SpanContext(from_header=False)
 
-        enabled = bool(int(trace_option) & _ENABLED_BITMASK)
+        # data.trace_id is in bytes with length 16, heclify it to hex bytes
+        # with length 32, then decode it to hex string using utf-8.
+        trace_id = binascii.hexlify(data.trace_id).decode(UTF8)
+        span_id = data.span_id
+        trace_option = data.trace_option
+
+        enabled = bool(trace_option & _ENABLED_BITMASK)
 
         span_context = SpanContext(
                 trace_id=trace_id,
@@ -112,17 +133,22 @@ class BinaryFormatPropagator(object):
         span_id = span_context.span_id
         enabled = span_context.enabled
 
-        # Pad zeroes to span_id if length less than 16
-        span_id_str = str(span_id).zfill(16)
+        # If there is no span_id in this context, set it to 0, which is
+        # considered invalid and won't be set as the downstream parent span_id.
+        if span_id is None:
+            span_id = '0'
 
-        enabled_str = str(enabled)
+        enabled_int = int(enabled)
 
+        # Convert trace_id to bytes with length 16, treat span_id as 64 bit
+        # integer which is unsigned long long type and convert it to bytes with
+        # length 8, trace_option is integer with length 1.
         return struct.pack(
             BINARY_FORMAT,
             VERSION_ID,
             TRACE_ID_FIELD_ID,
-            bytes(trace_id, UTF8),
+            binascii.unhexlify(trace_id),
             SPAN_ID_FIELD_ID,
-            bytes(span_id_str, UTF8),
+            span_id,
             TRACE_OPTION_FIELD_ID,
-            bytes(enabled_str, UTF8))
+            enabled_int)
