4c66047bc63849f19aea02ff5c8d03f954db37c1
Angela Li <yanhuil@google.com>
2017-09-28 15:03:36 -0700

    Make tracer params configurable in django middleware

--- trace/opencensus/trace/ext/django/config.py
+++ trace/opencensus/trace/ext/django/config.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 import importlib
+import logging
 
 from django.conf import settings as django_settings
 
@@ -23,7 +24,16 @@ DEFAULT_DJANGO_TRACER_CONFIG = {
                   'GoogleCloudFormatPropagator',
 }
 
+DEFAULT_DJANGO_TRACER_PARAMS = {
+    'SAMPLING_RATE': 0.5,
+    'GCP_REPORTER_PROJECT': 'my_project',
+    'ZIPKIN_REPORTER_SERVICE_NAME': 'my_service',
+}
+
+
 PATH_DELIMITER = '.'
+
+log = logging.getLogger(__name__)
 
 
 class DjangoTraceSettings(object):
@@ -38,6 +48,11 @@ class DjangoTraceSettings(object):
             django_settings,
             'OPENCENSUS_TRACE',
             DEFAULT_DJANGO_TRACER_CONFIG)
+
+        self.params = getattr(
+            django_settings,
+            'OPENCENSUS_TRACE_PARAMS',
+            DEFAULT_DJANGO_TRACER_PARAMS)
 
     def __getattr__(self, attr):
         # If not in defaults, it is something we cannot parse.
@@ -57,9 +72,15 @@ def convert_to_import(path):
     class to import.
     """
     # Split the path string to module name and class name
-    module_name, class_name = path.rsplit(PATH_DELIMITER, 1)
-    module = importlib.import_module(module_name)
-    return getattr(module, class_name)
+    try:
+        module_name, class_name = path.rsplit(PATH_DELIMITER, 1)
+        module = importlib.import_module(module_name)
+        return getattr(module, class_name)
+    except (ImportError, AttributeError):
+        msg = 'Failed to import {}'.format(path)
+        log.error(msg)
+
+        raise ImportError(msg)
 
 
 settings = DjangoTraceSettings()
