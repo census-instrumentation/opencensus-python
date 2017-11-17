abe9a699546871740755099874e39d1c83e01e72
Angela Li <yanhuil@google.com>
2017-11-16 16:05:29 -0800

    Add hello world example (#73)

---
+++ examples/trace/helloworld/flask/simple.py
@@ -0,0 +1,33 @@
+# Copyright 2017, OpenCensus Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import flask
+
+from opencensus.trace.exporters import stackdriver_exporter
+from opencensus.trace.ext.flask.flask_middleware import FlaskMiddleware
+
+
+app = flask.Flask(__name__)
+
+# Enable tracing the requests
+middleware = FlaskMiddleware(app)
+
+
+@app.route('/')
+def hello():
+    return 'Hello world!'
+
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=8080)
