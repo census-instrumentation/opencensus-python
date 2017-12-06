0d9e60dd7a8b6c202f0062eef2a5a3b7f477249a
Angela Li <yanhuil@google.com>
2017-12-06 13:43:31 -0800

    fix

--- tests/system/trace/basic_trace/basic_trace_system_test.py
+++ tests/system/trace/basic_trace/basic_trace_system_test.py
@@ -24,49 +24,49 @@ def func_to_trace():
 class TestBasicTrace(unittest.TestCase):
 
     def test_request_tracer(self):
-import json
+        import json
 
-from opencensus.trace import request_tracer
-from opencensus.trace.samplers import always_on
-from opencensus.trace.exporters import file_exporter
-from opencensus.trace.propagation import google_cloud_format
+        from opencensus.trace import request_tracer
+        from opencensus.trace.samplers import always_on
+        from opencensus.trace.exporters import file_exporter
+        from opencensus.trace.propagation import google_cloud_format
 
-trace_id = 'f8739df974a4481f98748cd92b27177d'
-span_id = '16971691944144156899'
-trace_option = 1
+        trace_id = 'f8739df974a4481f98748cd92b27177d'
+        span_id = '16971691944144156899'
+        trace_option = 1
 
-trace_header = '{}/{};o={}'.format(trace_id, span_id, trace_option)
+        trace_header = '{}/{};o={}'.format(trace_id, span_id, trace_option)
 
-sampler = always_on.AlwaysOnSampler()
-exporter = file_exporter.FileExporter()
-propagator = google_cloud_format.GoogleCloudFormatPropagator()
-span_context = propagator.from_header(header=trace_header)
+        sampler = always_on.AlwaysOnSampler()
+        exporter = file_exporter.FileExporter()
+        propagator = google_cloud_format.GoogleCloudFormatPropagator()
+        span_context = propagator.from_header(header=trace_header)
 
-tracer = request_tracer.RequestTracer(
-    span_context=span_context,
-    sampler=sampler,
-    exporter=exporter,
-    propagator=propagator
-)
+        tracer = request_tracer.RequestTracer(
+            span_context=span_context,
+            sampler=sampler,
+            exporter=exporter,
+            propagator=propagator
+        )
 
-with tracer.span(name='root_span') as root:
-    func_to_trace()
-    parent_span_id = root.span_id
-    with root.span(name='child_span'):
-        func_to_trace()
+        with tracer.span(name='root_span') as root:
+            func_to_trace()
+            parent_span_id = root.span_id
+            with root.span(name='child_span'):
+                func_to_trace()
 
-tracer.finish()
+        tracer.finish()
 
-file = open(file_exporter.DEFAULT_FILENAME, 'r')
-trace_json = json.loads(file.read())
+        file = open(file_exporter.DEFAULT_FILENAME, 'r')
+        trace_json = json.loads(file.read())
 
-spans = trace_json.get('spans')
+        spans = trace_json.get('spans')
 
-self.assertEqual(trace_json.get('traceId'), trace_id)
-self.assertEqual(len(spans), 2)
+        self.assertEqual(trace_json.get('traceId'), trace_id)
+        self.assertEqual(len(spans), 2)
 
-for span in spans:
-    if span.get('displayName').get('value') == 'root_span':
-        self.assertEqual(str(span.get('parentSpanId')), span_id)
-    else:
-        self.assertEqual(span.get('parentSpanId'), parent_span_id)
+        for span in spans:
+            if span.get('displayName').get('value') == 'root_span':
+                self.assertEqual(str(span.get('parentSpanId')), span_id)
+            else:
+                self.assertEqual(span.get('parentSpanId'), parent_span_id)
