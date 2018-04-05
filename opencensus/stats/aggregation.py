a8eff5a6dbdd6b544e38ef3b999f04f63d9d3ba2
kcooperstein <kcooperstein22@gmail.com>
2018-04-05 00:44:30 -0700

    Updating aggregation, aggregation data, and creating tag map

--- opencensus/stats/aggregation.py
+++ opencensus/stats/aggregation.py
@@ -1,38 +1,72 @@
 from opencensus.stats import bucket_boundaries
+from opencensus.stats import aggregation_data
 
 class BaseAggregation(object):
-    def __init__(self, aggregation=None):
-        self.aggregation = dict(aggregation or {})
+    def __init__(self, aggregation_type=None, buckets=None):
+        if aggregation_type is not None:
+            self.aggregation = aggregation_type
+
+        if buckets is not None:
+            self.buckets = buckets
 
     def get_aggregation(self):
         return self.aggregation
 
 class SumAggregation(BaseAggregation):
-    def __init__(self, instance):
-        self.instance = instance
+    def __init__(self, sum=None, aggregation_type="sum"):
+        super().__init__(aggregation_type)
+        self.aggregation_type = aggregation_type
+        if sum is not None:
+            self.sum = aggregation_data.SumAggregationDataFloat(sum)
+        else:
+            self.sum = aggregation_data.SumAggregationDataFloat(0)
 
-    def get_instance(self):
-        return self.instance
+    def get_aggregation(self):
+        return self.aggregation_type
+
+    def get_sum(self):
+        return self.sum
 
 class CountAggregation(BaseAggregation):
-    def __init__(self, instance):
-        self.instance = instance
+    def __init__(self, count=None, aggregation_type="count"):
+        super().__init__(aggregation_type)
+        self.aggregation_type = aggregation_type
+        if count is not None:
+            self.count = aggregation_data.CountAggregationData(count)
+        else:
+            self.count = aggregation_data.CountAggregationData(0)
 
-    def get_instance(self):
-        return self.instance
+    def get_aggregation(self):
+        return self.aggregation_type
+
+    def get_count(self):
+        return self.count
 
 class MeanAggregation(BaseAggregation):
-    def __init__(self, instance):
-        self.instance = instance
+    def __init__(self, mean=None, aggregation_type="mean"):
+        super().__init__(aggregation_type)
+        self.aggregation_type = aggregation_type
+        if mean is not None:
+            self.mean = aggregation_data.MeanAggregationData(mean)
+        else:
+            self.mean = aggregation_data.MeanAggregationData(0)
 
-    def get_instance(self):
-        return self.instance
+    def get_aggregation(self):
+        return self.aggregation_type
+
+    def get_mean(self):
+        return self.mean
 
 class DistributionAggregation(BaseAggregation):
-    def __init__(self, boundaries=None, distribution=None):
+    def __init__(self, boundaries=None, distribution=None, aggregation_type="distribution"):
+        super().__init__(aggregation_type, boundaries)
+        self.aggregation_type = aggregation_type
         '''self.boundaries = list(bucket_boundaries.BucketBoundaries(boundaries) or [])'''
         self.boundaries = bucket_boundaries.BucketBoundaries(boundaries)
         self.distribution = dict(distribution or {})
+
+    def get_aggregation(self):
+        return self.aggregation_type
 
     def get_boundaries(self):
         return bucket_boundaries.BucketBoundaries.get_boundaries(self.boundaries)
--- opencensus/stats/aggregation_data.py
+++ opencensus/stats/aggregation_data.py
@@ -1,32 +1,38 @@
 class BaseAggregationData(object):
-    def __init__(self, aggregation_data=None):
-        self.aggregation_data = dict(aggregation_data or {})
-
-class SumAggregationDataInt(BaseAggregationData):
-    def __init__(self, sum):
-        self.sum = sum
-
-    def get_sum(self):
-        return self.sum
+    def __init__(self, aggregation_data):
+        self.aggregation_data = aggregation_data
 
 class SumAggregationDataFloat(BaseAggregationData):
-    def __init__(self, sum):
-        self.sum = sum
+    def __init__(self, sum_data):
+        super().__init__(sum_data)
+        self.sum = sum_data
+
+    def add_sample(self, value):
+        self.sum = self.sum + value
 
     def get_sum(self):
         return self.sum
 
 class CountAggregationData(BaseAggregationData):
-    def __init__(self, count):
-        self.count = count
+    def __init__(self, count_data):
+        super().__init__(count_data)
+        self.count = count_data
+
+    def add_sample(self, value):
+        self.count = self.count + 1
 
     def get_count(self):
         return self.count
 
 class MeanAggregationData(BaseAggregationData):
-    def __init__(self, mean, count):
-        self.mean = mean
-        self.count = count
+    def __init__(self, mean_data, count_data):
+        super().__init__(mean_data)
+        self.mean = mean_data
+        self.count = count_data
+
+    def add_sample(self, value):
+        self.count = self.count + 1
+        self.mean = (self.mean * value) / self.count
 
     def get_mean(self):
         return self.mean
@@ -35,10 +41,49 @@ class MeanAggregationData(BaseAggregationData):
         return self.count
 
 class DistributionAggregationData(BaseAggregationData):
-    def __init__(self, mean, count, min, max, sum_of_sqd_deviations, counts_per_bucket):
-        self.mean = mean
-        self.count = count
+    def __init__(self, mean_data, count_data, min, max, sum_of_sqd_deviations, counts_per_bucket, bounds):
+        super().__init__(mean_data)
+        self.mean = mean_data
+        self.count = count_data
         self.min = min
         self.max = max
         self.sum_of_sqd_deviations = sum_of_sqd_deviations
         self.counts_per_bucket = counts_per_bucket
+        self.bounds = bounds
+
+    def get_sum(self):
+        return self.mean * self.count
+
+    def get_variance(self):
+        if self.count <= 1:
+            return 0
+        return self.sum_of_sqd_deviations / (self.count - 1)
+
+    def add_sample(self, value):
+        if value < self.min:
+            self.min = value
+        if value > self.max:
+            self.max = value
+        self.count += 1
+        self.increment_bucket_count(value)
+
+        if self.count == 1:
+            self.mean = value
+            return
+
+        old_mean = self.mean
+        self.mean = self.mean + (value-self.mean)/self.count
+        self.sum_of_sqd_deviations = self.sum_of_sqd_deviations + (value - old_mean)*(value - self.mean)
+
+    def increment_bucket_count(self, value):
+        if len(self.bounds) == 0:
+            self.counts_per_bucket[0] += 1
+            return
+
+        for i, b in self.bounds:
+            if value < b:
+                self.counts_per_bucket[i] += 1
+                return
+
+        self.counts_per_bucket[len(self.bounds)] += 1
+
