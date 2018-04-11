c0ea82812e31d8a2ecd4b923357bb30c529192a1
Angela Li <yanhuil@google.com>
2018-04-10 17:06:27 -0700

    Update nox config to run on both trace and stats(PR pending) (#153)

--- opencensus/__init__.py
+++ opencensus/__init__.py
@@ -15,6 +15,6 @@
 try:
     import pkg_resources
     pkg_resources.declare_namespace(__name__)
-except ImportError:
+except ImportError:  # pragma: NO COVER
     import pkgutil
     __path__ = pkgutil.extend_path(__path__, __name__)
