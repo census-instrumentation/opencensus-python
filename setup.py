d2a83207cd4c5d258362de6d9d18b42c40f9d71a
Angela Li <yanhuil@google.com>
2018-05-21 23:50:36 -0700

    Prepare for opencensus 0.1.5 release (#178)

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ install_requires = [
 
 setup(
     name='opencensus',
-    version='0.1.4',
+    version='0.1.5',
     author='OpenCensus Authors',
     author_email='census-developers@googlegroups.com',
     classifiers=[
