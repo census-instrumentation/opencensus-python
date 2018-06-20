3b135d03e58ab523f24d173de6487c472147aee2
Peter J. Bwire <bwire@caricah.com>
2018-06-21 00:03:10 +0300

    import correct trace module

--- opencensus/trace/ext/mysqldb/__init__.py
+++ opencensus/trace/ext/mysqldb/__init__.py
@@ -12,6 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from opencensus.trace.ext.pymysql import trace
+from opencensus.trace.ext.mysqldb import trace
 
 __all__ = ['trace']
