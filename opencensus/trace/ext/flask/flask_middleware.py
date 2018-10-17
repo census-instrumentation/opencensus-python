da07a2addb57a9d99a05e0a3d62a65f49fb3494f
Geoffrey Beausire <g.beausire@criteo.com>
2018-10-17 16:06:43 +0200

    Make the service name configurable for Jaeger in flask and django

--- opencensus/trace/ext/flask/flask_middleware.py
+++ opencensus/trace/ext/flask/flask_middleware.py
@@ -157,6 +157,11 @@ class FlaskMiddleware(object):
                 service_name=_service_name,
                 endpoint=_endpoint,
                 transport=transport)
+        elif self._exporter.__name__ == 'JaegerExporter':
+            _service_name = self._get_service_name(settings.params)
+            self.exporter = self._exporter(
+                service_name=_service_name,
+                transport=transport)
         else:
             self.exporter = self.exporter(transport=transport)
 
