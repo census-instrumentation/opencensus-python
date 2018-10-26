cc004e829da086bcb6e91f0d14935fdf62a80dcf
Geoffrey Beausire <g.beausire@criteo.com>
2018-10-26 14:57:37 +0200

    Add threadpool executor

--- opencensus/trace/ext/threading/trace.py
+++ opencensus/trace/ext/threading/trace.py
@@ -15,8 +15,11 @@
 import logging
 import threading
 from multiprocessing import pool
+from concurrent import futures
 
 from opencensus.trace import execution_context
+from opencensus.trace import tracer
+from opencensus.trace.propagation import binary_format
 
 log = logging.getLogger(__name__)
 
@@ -41,6 +44,11 @@ def trace_integration(tracer=None):
     setattr(pool.Pool, apply_async_func.__name__,
             wrap_apply_async(apply_async_func))
 
+    # Wrap the threading run function
+    submit_func = getattr(futures.ThreadPoolExecutor, "submit")
+    setattr(futures.ThreadPoolExecutor, submit_func.__name__,
+            wrap_submit(submit_func))
+
 
 def wrap_threading_start(start_func):
     """Wrap the start function from thread. Put the tracer informations in the
@@ -61,7 +69,9 @@ def wrap_threading_run(run_func):
 
     def call(self):
         execution_context.set_opencensus_full_context(*self._opencensus_context)
-        return run_func(self)
+        _tracer = execution_context.get_opencensus_tracer()
+        with _tracer.span(name=threading.current_thread().name):
+            return run_func(self)
 
     return call
 
@@ -71,22 +81,61 @@ def wrap_apply_async(apply_async_func):
     that will be called and wrap it then add the opencensus context."""
 
     def call(self, func, args=(), kwds={}, **kwargs):
-        func = wrap_task_func(func)
-        kwds['_opencensus_context'] = execution_context.get_opencensus_full_context()
-        return apply_async_func(self, func, args=args, kwds=kwds, **kwargs)
+        wrapped_func = wrap_task_func(func)
+        _tracer = execution_context.get_opencensus_tracer()
+        propagator = binary_format.BinaryFormatPropagator()
+
+        wrapped_kwargs = {}
+        wrapped_kwargs['span_context_binary'] = propagator.to_header(_tracer.span_context)
+        wrapped_kwargs['kwds'] = kwds
+        wrapped_kwargs['sampler'] = _tracer.sampler
+        wrapped_kwargs['exporter'] = _tracer.exporter
+        wrapped_kwargs['propagator'] = _tracer.propagator
+
+        return apply_async_func(self, wrapped_func, args=args, kwds=wrapped_kwargs, **kwargs)
 
     return call
 
 
-def wrap_task_func(task_func):
+def wrap_submit(submit_func):
+    """Wrap the apply_async function of multiprocessing.pools. Get the function
+    that will be called and wrap it then add the opencensus context."""
+
+    def call(self, func, *args, **kwargs):
+        wrapped_func = wrap_task_func(func)
+        _tracer = execution_context.get_opencensus_tracer()
+        propagator = binary_format.BinaryFormatPropagator()
+
+        wrapped_kwargs = {}
+        wrapped_kwargs['span_context_binary'] = propagator.to_header(_tracer.span_context)
+        wrapped_kwargs['kwds'] = kwargs
+        wrapped_kwargs['sampler'] = _tracer.sampler
+        wrapped_kwargs['exporter'] = _tracer.exporter
+        wrapped_kwargs['propagator'] = _tracer.propagator
+
+        return submit_func(self, wrapped_func, *args, **wrapped_kwargs)
+
+    return call
+
+
+class wrap_task_func(object):
     """Wrap the function given to apply_async to get the tracer from context,
     execute the function then clear the context."""
+    def __init__(self, func):
+        self.func = func
 
-    def call(*args, **kwargs):
-        _opencensus_context = kwargs.pop('_opencensus_context')
-        execution_context.set_opencensus_full_context(*_opencensus_context)
-        result = task_func(*args, **kwargs)
+    def __call__(self, *args, **kwargs):
+        if 'kwds' not in kwargs:
+            return self.func(*args, **kwargs)
+        kwds = kwargs.pop('kwds')
+
+        span_context_binary = kwargs.pop('span_context_binary')
+        propagator = binary_format.BinaryFormatPropagator()
+        kwargs['span_context'] = propagator.from_header(span_context_binary)
+
+        _tracer = tracer.Tracer(**kwargs)
+        execution_context.set_opencensus_tracer(_tracer)
+        with _tracer.span(name=threading.current_thread().name):
+            result = self.func(*args, **kwds)
         execution_context.clean()
         return result
-
-    return call
