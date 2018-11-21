da8f22a0ca255154ffda165ead2413cef3d7e563
Chris Kleinknecht <libc@google.com>
2018-11-21 14:22:28 -0800

    s/0.1.8/0.1.9/

--- opencensus/trace/exporters/stackdriver_exporter.py
+++ opencensus/trace/exporters/stackdriver_exporter.py
@@ -25,7 +25,7 @@ from opencensus.common.monitored_resource_util.monitored_resource_util \
     import MonitoredResourceUtil
 
 # OpenCensus Version
-VERSION = '0.1.8'
+VERSION = '0.1.9'
 
 # Agent
 AGENT = 'opencensus-python [{}]'.format(VERSION)
