ee45c3c2c16785ead69d0e949d05bc347df87ac6
William Stewart <zoidbergwill@gmail.com>
2018-12-03 19:47:24 +0100

    WIP Django DB instrumentation tests

---
+++ tests/unit/trace/ext/django/test_db.py
@@ -0,0 +1,77 @@
+# Copyright 2017, OpenCensus Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+from collections import namedtuple
+
+import django
+import mock
+import pytest
+from django.test.utils import teardown_test_environment
+
+from opencensus.trace import execution_context
+from opencensus.trace.tracers.noop_tracer import NoopTracer
+
+
+class TestOpencensusDatabaseMiddleware(unittest.TestCase):
+    def setUp(self):
+        from django.conf import settings as django_settings
+        from django.test.utils import setup_test_environment
+
+
+        if not django_settings.configured:
+            django_settings.configure()
+        setup_test_environment()
+
+    def tearDown(self):
+        execution_context.clear()
+        teardown_test_environment()
+
+    def test_process_request(self):
+        if django.VERSION < (2, 0):
+            pytest.skip("Wrong version of Django")
+
+        from opencensus.trace.ext.django import middleware
+
+        tracer = middleware._get_current_tracer()
+        span = tracer.current_span()
+
+        sql = "SELECT * FROM users"
+
+        MockConnection = namedtuple('Connection', ('vendor'))
+        connection = MockConnection('mysql')
+
+        mock_execute = mock.Mock()
+        mock_execute.return_value = "Mock result"
+
+        middleware_obj = middleware.OpencensusMiddleware()
+
+        result = middleware_obj._trace_db_call(
+            mock_execute, sql, params=[], many=False,
+            context={'connection': connection})
+
+        mock_sql, mock_params, mock_many, mock_context = mock_execute.call_args[0]
+
+        self.assertEqual(mock_sql, sql)
+        self.assertEqual(mock_params, [])
+        self.assertEqual(mock_many, False)
+        self.assertEqual(mock_context, {'connection': connection})
+        self.assertEqual(result, "Mock result")
+
+        result = middleware_obj._trace_db_call(
+            mock_execute, sql, params=[], many=True,
+            context={'connection': connection})
+
+        mock_sql, mock_params, mock_many, mock_context = mock_execute.call_args[0]
+        self.assertEqual(mock_many, True)
