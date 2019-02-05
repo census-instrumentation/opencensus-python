f669aa9553b120e2b454fed6581ad13db8818ccc
Chris Kleinknecht <libc@google.com>
2019-02-05 09:59:44 -0800

    Metrics: add MetricsProducer and convert stats (#476)
    Add MetricProducer and MetricProducerManager classes and make Stats implement
    MetricProducer. Also fix a bug in DistributionAggregationData histogram point
    conversions.

--- opencensus/stats/stats.py
+++ opencensus/stats/stats.py
@@ -12,24 +12,29 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from datetime import datetime
+
+from opencensus.metrics.export.metric_producer import MetricProducer
 from opencensus.stats.stats_recorder import StatsRecorder
 from opencensus.stats.view_manager import ViewManager
 
 
-class Stats(object):
+class Stats(MetricProducer):
     """Stats defines a View Manager and a Stats Recorder in order for the
     collection of Stats
     """
+
     def __init__(self):
-        self._stats_recorder = StatsRecorder()
-        self._view_manager = ViewManager()
+        self.stats_recorder = StatsRecorder()
+        self.view_manager = ViewManager()
 
-    @property
-    def stats_recorder(self):
-        """the current stats recorder for Stats"""
-        return self._stats_recorder
+    def get_metrics(self):
+        """Get a Metric for each of the view manager's registered views.
 
-    @property
-    def view_manager(self):
-        """the current view manager for Stats"""
-        return self._view_manager
+        Convert each registered view's associated `ViewData` into a `Metric` to
+        be exported, using the current time for metric conversions.
+
+        :rtype: Iterator[:class: `opencensus.metrics.export.metric.Metric`]
+        """
+        return self.view_manager.measure_to_view_map.get_metrics(
+            datetime.now())
