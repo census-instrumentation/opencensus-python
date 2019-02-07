0a84bb1568519d9156843bed4772a41167e62e3f
Chris Kleinknecht <libc@google.com>
2019-02-06 16:33:10 -0800

    Add gauge module

---
+++ opencensus/metrics/export/gauge.py
@@ -0,0 +1,202 @@
+# Copyright 2019, OpenCensus Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from collections import OrderedDict
+import six
+import threading
+
+from opencensus.metrics.export import metric
+from opencensus.metrics.export import metric_descriptor
+from opencensus.metrics.export import point as point_module
+from opencensus.metrics.export import time_series
+from opencensus.metrics.export import value as value_module
+
+
+class GaugePointLong(object):
+    """An instantaneous measurement from a LongGauge.
+
+    A GaugePointLong represents the most recent measurement from a
+    :class:`LongGauge` for a given set of label values.
+    """
+
+    def __init__(self):
+        self.value = 0
+        self._value_lock = threading.Lock()
+
+    def __repr__(self):
+        return ("{}({})"
+                .format(
+                    type(self).__name__,
+                    self.value
+                ))
+
+    def add(self, val):
+        """Add `val` to the current value.
+
+        :type val: int
+        :param val: Value to add.
+        """
+        if not isinstance(val, six.integer_types):
+            raise ValueError("GaugePointLong only supports integer types")
+        with self._value_lock:
+            self.value += val
+
+    def set(self, val):
+        """Set the current value to `val`.
+
+        :type val: int
+        :param val: Value to set.
+        """
+        if not isinstance(val, six.integer_types):
+            raise ValueError("GaugePointLong only supports integer types")
+        with self._value_lock:
+            self.value = val
+
+
+class GaugePointDouble(object):
+    """An instantaneous measurement from a DoubleGauge.
+
+    A GaugePointDouble represents the most recent measurement from a
+    :class:`DoubleGauge` for a given set of label values.
+    """
+
+    def __init__(self):
+        self.value = 0.0
+        self._value_lock = threading.Lock()
+
+    def __repr__(self):
+        return ("{}({})"
+                .format(
+                    type(self).__name__,
+                    self.value
+                ))
+
+    def add(self, val):
+        """Add `val` to the current value.
+
+        :type val: float
+        :param val: Value to add.
+        """
+        with self._value_lock:
+            self.value += val
+
+    def set(self, val):
+        """Set the current value to `val`.
+
+        :type val: float
+        :param val: Value to set.
+        """
+        with self._value_lock:
+            self.value = float(val)
+
+
+class Gauge(object):
+    """A set of instantaneous measurements of the same type.
+
+    End users should use :class:`LongGauge` or :class:`DoubleGauge` instead of
+    using this class directly.
+
+    The constructor arguments are used to create a
+    :class:`opencensus.metrics.export.metric_descriptor.MetricDescriptor` for
+    converted metrics. See that class for details.
+    """
+
+    def __init__(self, name, description, unit, label_keys):
+        self._len_label_keys = len(label_keys)
+        self.descriptor = metric_descriptor.MetricDescriptor(
+            name, description, unit, self.descriptor_type, label_keys)
+        self.points = OrderedDict()
+        self._points_lock = threading.Lock()
+
+    def __repr__(self):
+        return ('{}(descriptor.name="{}", points={})'
+                .format(
+                    type(self).__name__,
+                    self.descriptor.name,
+                    self.points
+                ))
+
+    def get_time_series(self, label_values):
+        """Get a mutable measurement for the given set of label values.
+
+        :type label_values: list(:class:`LabelValue`)
+        :param label_values: The measurement's label values.
+
+        :rtype: :class:`GaugePointLong` or :class:`GaugePointDouble`
+        :return: A mutable point that represents the last value of the
+        measurement.
+        """
+        if label_values is None:
+            raise ValueError
+        if any(lv is None for lv in label_values):
+            raise ValueError
+        if len(label_values) != self._len_label_keys:
+            raise ValueError
+        with self._points_lock:
+            return self.points.setdefault(
+                tuple(label_values), self.point_type())
+
+    def clear(self):
+        """Remove all points from this gauge."""
+        with self._points_lock:
+            self.points = OrderedDict()
+
+    def get_metric(self, timestamp):
+        """Get a metric including all current time series.
+
+        Get a :class:`opencensus.metrics.export.metric.Metric` with one
+        :class:`opencensus.metrics.export.time_series.TimeSeries` for each
+        set of label values with a recorded measurement. Each `TimeSeries`
+        has a single point that represents the last recorded value.
+
+        :type timestamp: :class:`datetime.datetime`
+        :param timestamp: Recording time to report, usually the current time.
+
+        :rtype: :class:`opencensus.metrics.export.metric.Metric` or None
+        :return: A converted metric for all current measurements.
+        """
+        if not self.points:
+            return None
+
+        ts_list = []
+        for lv, gp in self.points.items():
+            point = point_module.Point(self.value_type(gp.value), timestamp)
+            ts_list.append(time_series.TimeSeries(lv, [point], timestamp))
+        return metric.Metric(self.descriptor, ts_list)
+
+    @property
+    def descriptor_type(self):  # pragma: NO COVER
+        raise NotImplementedError
+
+    @property
+    def point_type(self):  # pragma: NO COVER
+        raise NotImplementedError
+
+    @property
+    def value_type(self):  # pragma: NO COVER
+        raise NotImplementedError
+
+
+class LongGauge(Gauge):
+    """Gauge for recording int-valued measurements."""
+    descriptor_type = metric_descriptor.MetricDescriptorType.GAUGE_INT64
+    point_type = GaugePointLong
+    value_type = value_module.ValueLong
+
+
+class DoubleGauge(Gauge):
+    """Gauge for recording float-valued measurements."""
+    descriptor_type = metric_descriptor.MetricDescriptorType.GAUGE_DOUBLE
+    point_type = GaugePointDouble
+    value_type = value_module.ValueDouble
---
+++ tests/unit/metrics/export/test_gauge.py
@@ -0,0 +1,202 @@
+# Copyright 2019, OpenCensus Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+try:
+    from mock import Mock
+except ImportError:
+    from unittest.mock import Mock
+
+import unittest
+
+from opencensus.metrics.export import gauge
+from opencensus.metrics.export import metric_descriptor
+from opencensus.metrics.export import value as value_module
+
+
+class TestGaugePointLong(unittest.TestCase):
+    def test_init(self):
+        point = gauge.GaugePointLong()
+        self.assertEqual(point.value, 0)
+        self.assertIsInstance(point.value, int)
+
+    def test_add(self):
+        point = gauge.GaugePointLong()
+        point.add(10)
+        self.assertEqual(point.value, 10)
+        point.add(-20)
+        self.assertEqual(point.value, -10)
+        with self.assertRaises(ValueError):
+            point.add(10.0)
+
+    def test_set(self):
+        point = gauge.GaugePointLong()
+        point.set(10)
+        self.assertEqual(point.value, 10)
+        point.set(-20)
+        self.assertEqual(point.value, -20)
+        with self.assertRaises(ValueError):
+            point.set(10.0)
+
+
+class TestGaugePointDouble(unittest.TestCase):
+    def test_init(self):
+        point = gauge.GaugePointDouble()
+        self.assertEqual(point.value, 0.0)
+        self.assertIsInstance(point.value, float)
+
+    def test_add(self):
+        point = gauge.GaugePointDouble()
+        point.add(10)
+        self.assertEqual(point.value, 10.0)
+        point.add(-20.2)
+        self.assertEqual(point.value, -10.2)
+
+    def test_set(self):
+        point = gauge.GaugePointDouble()
+        point.set(10)
+        self.assertEqual(point.value, 10.0)
+        point.set(-20.2)
+        self.assertEqual(point.value, -20.2)
+
+
+class TestLongGauge(unittest.TestCase):
+    def test_init(self):
+        name = Mock()
+        description = Mock()
+        unit = Mock()
+        label_keys = [Mock(), Mock]
+        long_gauge = gauge.LongGauge(name, description, unit, label_keys)
+        self.assertEqual(long_gauge.descriptor.name, name)
+        self.assertEqual(long_gauge.descriptor.description, description)
+        self.assertEqual(long_gauge.descriptor.unit, unit)
+        self.assertEqual(long_gauge.descriptor.label_keys, label_keys)
+        self.assertEqual(long_gauge.descriptor.type,
+                         metric_descriptor.MetricDescriptorType.GAUGE_INT64)
+
+    def test_get_time_series(self):
+        name = Mock()
+        description = Mock()
+        unit = Mock()
+        label_keys = [Mock(), Mock]
+        long_gauge = gauge.LongGauge(name, description, unit, label_keys)
+        with self.assertRaises(ValueError):
+            long_gauge.get_time_series(None)
+        with self.assertRaises(ValueError):
+            long_gauge.get_time_series([Mock()])
+        with self.assertRaises(ValueError):
+            long_gauge.get_time_series([Mock(), Mock(), Mock()])
+        with self.assertRaises(ValueError):
+            long_gauge.get_time_series([Mock(), None])
+
+        label_values = [Mock(), Mock()]
+        point = long_gauge.get_time_series(label_values)
+        self.assertIsInstance(point, gauge.GaugePointLong)
+        self.assertEqual(point.value, 0)
+        self.assertEqual(len(long_gauge.points.keys()), 1)
+        [key] = long_gauge.points.keys()
+        self.assertEqual(key, tuple(label_values))
+        point2 = long_gauge.get_time_series(label_values)
+        self.assertIs(point, point2)
+        self.assertEqual(len(long_gauge.points.keys()), 1)
+
+    def test_clear(self):
+        name = Mock()
+        description = Mock()
+        unit = Mock()
+        label_keys = [Mock(), Mock]
+        long_gauge = gauge.LongGauge(name, description, unit, label_keys)
+
+        label_values = [Mock(), Mock()]
+        point = long_gauge.get_time_series(label_values)
+        self.assertEqual(len(long_gauge.points.keys()), 1)
+        point.add(1)
+
+        long_gauge.clear()
+        self.assertDictEqual(long_gauge.points, {})
+
+        label_values = [Mock(), Mock()]
+        point2 = long_gauge.get_time_series(label_values)
+        self.assertEqual(point2.value, 0)
+        self.assertIsNot(point, point2)
+
+    def test_get_metric(self):
+        name = Mock()
+        description = Mock()
+        unit = Mock()
+        label_keys = [Mock(), Mock]
+        long_gauge = gauge.LongGauge(name, description, unit, label_keys)
+
+        timestamp = Mock()
+        null_metric = long_gauge.get_metric(timestamp)
+        self.assertIsNone(null_metric)
+
+        lv1 = [Mock(), Mock()]
+        lv2 = [Mock(), Mock()]
+        point1 = long_gauge.get_time_series(lv1)
+        point2 = long_gauge.get_time_series(lv2)
+
+        point1.set(1)
+        point2.set(2)
+        metric = long_gauge.get_metric(timestamp)
+        self.assertEqual(metric.descriptor, long_gauge.descriptor)
+        self.assertEqual(len(metric.time_series), 2)
+        self.assertEqual(len(metric.time_series[0].points), 1)
+        self.assertEqual(len(metric.time_series[1].points), 1)
+        self.assertIsInstance(metric.time_series[0].points[0].value,
+                              value_module.ValueLong)
+        self.assertIsInstance(metric.time_series[1].points[0].value,
+                              value_module.ValueLong)
+        self.assertEqual(metric.time_series[0].points[0].value.value, 1)
+        self.assertEqual(metric.time_series[1].points[0].value.value, 2)
+
+
+class TestDoubleGauge(unittest.TestCase):
+
+    # TestLongGauge does the heavy lifting, this test just checks that
+    # DoubleGauge creates points and metrics of the right type
+    def test_init(self):
+        name = Mock()
+        description = Mock()
+        unit = Mock()
+        label_keys = [Mock(), Mock]
+        double_gauge = gauge.DoubleGauge(name, description, unit, label_keys)
+        self.assertEqual(double_gauge.descriptor.type,
+                         metric_descriptor.MetricDescriptorType.GAUGE_DOUBLE)
+
+    def test_get_time_series(self):
+        double_gauge = gauge.DoubleGauge(Mock(), Mock(), Mock(),
+                                         [Mock(), Mock])
+        point = double_gauge.get_time_series([Mock(), Mock()])
+        self.assertIsInstance(point, gauge.GaugePointDouble)
+
+    def test_get_metric(self):
+        double_gauge = gauge.DoubleGauge(Mock(), Mock(), Mock(),
+                                         [Mock(), Mock])
+
+        timestamp = Mock()
+
+        lv1 = [Mock(), Mock()]
+        lv2 = [Mock(), Mock()]
+        point1 = double_gauge.get_time_series(lv1)
+        point2 = double_gauge.get_time_series(lv2)
+
+        point1.set(1.2)
+        point2.set(2.2)
+        metric = double_gauge.get_metric(timestamp)
+        self.assertIsInstance(metric.time_series[0].points[0].value,
+                              value_module.ValueDouble)
+        self.assertIsInstance(metric.time_series[1].points[0].value,
+                              value_module.ValueDouble)
+        self.assertEqual(metric.time_series[0].points[0].value.value, 1.2)
+        self.assertEqual(metric.time_series[1].points[0].value.value, 2.2)
