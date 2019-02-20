e3c8b934d2eb60664504191201515872a7c6ab7d
Chris Kleinknecht <libc@google.com>
2019-02-20 13:43:38 -0800

    Update monitored resource docstrings

--- opencensus/common/monitored_resource_util/monitored_resource.py
+++ opencensus/common/monitored_resource_util/monitored_resource.py
@@ -14,12 +14,12 @@
 
 import os
 
+from opencensus.common import resource
 from opencensus.common.monitored_resource_util import aws_identity_doc_utils
 from opencensus.common.monitored_resource_util import gcp_metadata_config
-from opencensus.common import resource
 
 
-# supported environments (resource types)
+# Supported environments (resource types)
 _GCE_INSTANCE = "gce_instance"
 _GKE_CONTAINER = "gke_container"
 _AWS_EC2_INSTANCE = "aws_ec2_instance"
@@ -29,46 +29,40 @@ _KUBERNETES_SERVICE_HOST = 'KUBERNETES_SERVICE_HOST'
 
 
 def is_gke_environment():
-    """A Google Container Engine (GKE) container instance.
-    KUBERNETES_SERVICE_HOST environment variable must be set.
+    """Whether the environment is a GKE container instance.
+
+    The KUBERNETES_SERVICE_HOST environment variable must be set.
     """
     return _KUBERNETES_SERVICE_HOST in os.environ
 
 
 def is_gce_environment():
-    """A virtual machine instance hosted in Google Compute Engine (GCE)."""
+    """Whether the environment is a virtual machine on GCE."""
     return gcp_metadata_config.GcpMetadataConfig.is_running_on_gcp()
 
 
 def is_aws_environment():
-    """A virtual machine instance in Amazon EC2"""
+    """Whether the environment is a virtual machine instance on EC2."""
     return aws_identity_doc_utils.AwsIdentityDocumentUtils.is_running_on_aws()
 
 
 def get_instance():
-    """Get a monitored resource based on the application environment.
+    """Get a resource based on the application environment.
 
-    Returns a self-configured monitored resource, or None if the application is
-    not running on a supported environment.
+    Returns a `Resource` configured for the current environment, or None if the
+    environment is unknown or unsupported.
 
-    It supports following environments (resource types)
-    1. gke_container:
-    2. gce_instance:
-    3. aws_ec2_instance:
+    Supported environments include:
 
-    AwsMonitoredResource represents aws_ec2_instance type monitored resource.
-    For definition refer to
-    https://cloud.google.com/monitoring/api/resources#tag_aws_ec2_instance
+    1. 'gke_container'
+    - https://cloud.google.com/monitoring/api/resources#tag_gke_container
+    2. 'gce_instance'
+    - https://cloud.google.com/monitoring/api/resources#tag_gce_instance
+    3. 'aws_ec2_instance'
+    - https://cloud.google.com/monitoring/api/resources#tag_aws_ec2_instance
 
-    GkeMonitoredResource represents gke_container type monitored resource.
-    For definition refer to
-    https://cloud.google.com/monitoring/api/resources#tag_gke_container
-
-    GceMonitoredResource represents gce_instance type monitored resource.
-    For definition refer to
-    https://cloud.google.com/monitoring/api/resources#tag_gce_instance
-
-    :return: MonitoredResource or None
+    :rtype: :class:`opencensus.common.resource.Resource` or None
+    :return: A `Resource` configured for the current environment.
     """
     if is_gke_environment():
         return resource.Resource(
