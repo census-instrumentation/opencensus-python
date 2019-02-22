f814904b8656b4a4e9d1d423e908796798549ae3
Chris Kleinknecht <libc@google.com>
2019-02-22 11:46:23 -0800

    Value type lint fixes (#516)

--- opencensus/metrics/export/point.py
+++ opencensus/metrics/export/point.py
@@ -16,11 +16,14 @@
 class Point(object):
     """A timestamped measurement of a TimeSeries.
 
-    :type value: Value
-    :param value: the Value of the Point.
+    :type value: :class:`opencensus.metrics.export.value.ValueDouble` or
+        :class:`opencensus.metrics.export.value.ValueLong` or
+        :class:`opencensus.metrics.export.value.ValueSummary` or
+        :class:`opencensus.metrics.export.value.ValueDistribution`
+    :param value: the point value.
 
     :type timestamp: time
-    :param timestamp: the Timestamp when the Point was recorded.
+    :param timestamp: the timestamp when the `Point` was recorded.
     """
 
     def __init__(self, value, timestamp):
@@ -29,12 +32,10 @@ class Point(object):
 
     @property
     def value(self):
-        """Returns the Value"""
         return self._value
 
     @property
     def timestamp(self):
-        """Returns the Timestamp when this Point was recorded."""
         return self._timestamp
 
     def __repr__(self):
