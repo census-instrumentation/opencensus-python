6fb2b3e2fbfc0e94e3105462b98d61fbb1d916e2
songy23 <songya@google.com>
2019-02-25 16:48:16 -0800

    Regenerate ocagent proto files.

--- opencensus/trace/exporters/gen/opencensus/agent/trace/v1/trace_service_pb2_grpc.py
+++ opencensus/trace/exporters/gen/opencensus/agent/trace/v1/trace_service_pb2_grpc.py
@@ -5,63 +5,68 @@ from opencensus.trace.exporters.gen.opencensus.agent.trace.v1 import trace_servi
 
 
 class TraceServiceStub(object):
-    # missing associated documentation comment in .proto file
-    pass
+  """Service that can be used to push spans and configs between one Application
+  instrumented with OpenCensus and an agent, or between an agent and a
+  central collector or config service (in this case spans and configs are
+  sent/received to/from multiple Applications).
+  """
 
-    def __init__(self, channel):
-        """Constructor.
+  def __init__(self, channel):
+    """Constructor.
 
-        Args:
-          channel: A grpc.Channel.
-        """
-        self.Config = channel.stream_stream(
-            '/opencensus.proto.agent.trace.v1.TraceService/Config',
-            request_serializer=opencensus_dot_proto_dot_agent_dot_trace_dot_v1_dot_trace__service__pb2.CurrentLibraryConfig.SerializeToString,
-            response_deserializer=opencensus_dot_proto_dot_agent_dot_trace_dot_v1_dot_trace__service__pb2.UpdatedLibraryConfig.FromString,
+    Args:
+      channel: A grpc.Channel.
+    """
+    self.Config = channel.stream_stream(
+        '/opencensus.proto.agent.trace.v1.TraceService/Config',
+        request_serializer=opencensus_dot_proto_dot_agent_dot_trace_dot_v1_dot_trace__service__pb2.CurrentLibraryConfig.SerializeToString,
+        response_deserializer=opencensus_dot_proto_dot_agent_dot_trace_dot_v1_dot_trace__service__pb2.UpdatedLibraryConfig.FromString,
         )
-        self.Export = channel.stream_stream(
-            '/opencensus.proto.agent.trace.v1.TraceService/Export',
-            request_serializer=opencensus_dot_proto_dot_agent_dot_trace_dot_v1_dot_trace__service__pb2.ExportTraceServiceRequest.SerializeToString,
-            response_deserializer=opencensus_dot_proto_dot_agent_dot_trace_dot_v1_dot_trace__service__pb2.ExportTraceServiceResponse.FromString,
+    self.Export = channel.stream_stream(
+        '/opencensus.proto.agent.trace.v1.TraceService/Export',
+        request_serializer=opencensus_dot_proto_dot_agent_dot_trace_dot_v1_dot_trace__service__pb2.ExportTraceServiceRequest.SerializeToString,
+        response_deserializer=opencensus_dot_proto_dot_agent_dot_trace_dot_v1_dot_trace__service__pb2.ExportTraceServiceResponse.FromString,
         )
 
 
 class TraceServiceServicer(object):
-    # missing associated documentation comment in .proto file
-    pass
+  """Service that can be used to push spans and configs between one Application
+  instrumented with OpenCensus and an agent, or between an agent and a
+  central collector or config service (in this case spans and configs are
+  sent/received to/from multiple Applications).
+  """
 
-    def Config(self, request_iterator, context):
-        """After initialization, this RPC must be kept alive for the
-        entire life of the application. The agent pushes configs
-        down to applications via a stream.
-        """
-        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
-        context.set_details('Method not implemented!')
-        raise NotImplementedError('Method not implemented!')
+  def Config(self, request_iterator, context):
+    """After initialization, this RPC must be kept alive for the entire life of
+    the application. The agent pushes configs down to applications via a
+    stream.
+    """
+    context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+    context.set_details('Method not implemented!')
+    raise NotImplementedError('Method not implemented!')
 
-    def Export(self, request_iterator, context):
-        """Allows applications to send spans to the agent.
-        For performance reasons, it is recommended to keep this RPC
-        alive for the entire life of the application.
-        """
-        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
-        context.set_details('Method not implemented!')
-        raise NotImplementedError('Method not implemented!')
+  def Export(self, request_iterator, context):
+    """For performance reasons, it is recommended to keep this RPC
+    alive for the entire life of the application.
+    """
+    context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+    context.set_details('Method not implemented!')
+    raise NotImplementedError('Method not implemented!')
 
 
 def add_TraceServiceServicer_to_server(servicer, server):
-    rpc_method_handlers = {
-        'Config': grpc.stream_stream_rpc_method_handler(
-            servicer.Config,
-            request_deserializer=opencensus_dot_proto_dot_agent_dot_trace_dot_v1_dot_trace__service__pb2.CurrentLibraryConfig.FromString,
-            response_serializer=opencensus_dot_proto_dot_agent_dot_trace_dot_v1_dot_trace__service__pb2.UpdatedLibraryConfig.SerializeToString,
-        ),
-        'Export': grpc.stream_stream_rpc_method_handler(
-            servicer.Export,
-            request_deserializer=opencensus_dot_proto_dot_agent_dot_trace_dot_v1_dot_trace__service__pb2.ExportTraceServiceRequest.FromString,
-            response_serializer=opencensus_dot_proto_dot_agent_dot_trace_dot_v1_dot_trace__service__pb2.ExportTraceServiceResponse.SerializeToString,
-        ),
-    }
-    generic_handler = grpc.method_handlers_generic_handler(
-        'opencensus.proto.agent.trace.v1.TraceService', rpc_method_handlers)
-    server.add_generic_rpc_handlers((generic_handler,))
+  rpc_method_handlers = {
+      'Config': grpc.stream_stream_rpc_method_handler(
+          servicer.Config,
+          request_deserializer=opencensus_dot_proto_dot_agent_dot_trace_dot_v1_dot_trace__service__pb2.CurrentLibraryConfig.FromString,
+          response_serializer=opencensus_dot_proto_dot_agent_dot_trace_dot_v1_dot_trace__service__pb2.UpdatedLibraryConfig.SerializeToString,
+      ),
+      'Export': grpc.stream_stream_rpc_method_handler(
+          servicer.Export,
+          request_deserializer=opencensus_dot_proto_dot_agent_dot_trace_dot_v1_dot_trace__service__pb2.ExportTraceServiceRequest.FromString,
+          response_serializer=opencensus_dot_proto_dot_agent_dot_trace_dot_v1_dot_trace__service__pb2.ExportTraceServiceResponse.SerializeToString,
+      ),
+  }
+  generic_handler = grpc.method_handlers_generic_handler(
+      'opencensus.proto.agent.trace.v1.TraceService', rpc_method_handlers)
+  server.add_generic_rpc_handlers((generic_handler,))
