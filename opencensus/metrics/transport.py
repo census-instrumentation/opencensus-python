3cddbd664ebd775c9ebc13c4a63a5251f3ebe6cb
Chris Kleinknecht <libc@google.com>
2019-03-25 18:33:26 -0700

    Backwards-compatible queue import

--- opencensus/metrics/transport.py
+++ opencensus/metrics/transport.py
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from six.moves import queue
 import logging
-import queue
 import threading
 
 from opencensus.common import utils
