60c31047cf90e4839c7854dc9edcc0a02e4b4593
Will Sabransky <will.sabransky@postmates.com>
2019-04-04 14:20:30 -0700

    Prevent middleware error on older Django versions

--- contrib/opencensus-ext-django/opencensus/ext/django/middleware.py
+++ contrib/opencensus-ext-django/opencensus/ext/django/middleware.py
@@ -99,7 +99,11 @@ def _set_django_attributes(span, request):
         return
 
     user_id = django_user.pk
-    user_name = django_user.get_username()
+    try:
+        user_name = django_user.get_username()
+    except Exception:
+        # AnonymouUser in some older versions of Django doesn't implement get_username()
+        return
 
     # User id is the django autofield for User model as the primary key
     if user_id is not None:
