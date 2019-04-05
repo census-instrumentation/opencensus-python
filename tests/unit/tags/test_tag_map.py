922702b6ac61615c8e3674f152c5ee6e25ccc96d
Reiley Yang <reyang@microsoft.com>
2019-04-04 18:45:06 -0700

    Minor refactor Tags (#602)

--- tests/unit/tags/test_tag_map.py
+++ tests/unit/tags/test_tag_map.py
@@ -26,15 +26,15 @@ class TestTagMap(unittest.TestCase):
 
     def test_constructor_explicit(self):
         tags_list = [
-            tags.Tag(tags.TagKey('key1'), tags.TagValue('value1')),
-            tags.Tag(tags.TagKey('key2'), tags.TagValue('value2'))
+            tags.Tag('key1', 'value1'),
+            tags.Tag('key2', 'value2')
         ]
         tag_map = tags.TagMap(tags=tags_list)
         self.assertEqual(tag_map.map, dict(tags_list))
 
     def test_insert(self):
-        test_key = tags.TagKey('key1')
-        test_value = tags.TagValue('value1')
+        test_key = 'key1'
+        test_value = 'value1'
 
         tag_map = tags.TagMap()
         tag_map.insert(key=test_key, value=test_value)
@@ -47,9 +47,9 @@ class TestTagMap(unittest.TestCase):
             ValueError, tag_map.insert, key='Æ!01kr', value=test_value)
 
     def test_delete(self):
-        key = tags.TagKey('key1')
-        tag1 = tags.Tag(tags.TagKey('key1'), tags.TagValue('value1'))
-        tag2 = tags.Tag(tags.TagKey('key2'), tags.TagValue('value2'))
+        key = 'key1'
+        tag1 = tags.Tag('key1', 'value1')
+        tag2 = tags.Tag('key2', 'value2')
         tt = [tag1, tag2]
 
         tag_map = tags.TagMap(tags=tt)
@@ -57,25 +57,25 @@ class TestTagMap(unittest.TestCase):
         self.assertEqual(tag_map.map, {tag2.key: tag2.value})
 
     def test_update(self):
-        key_1 = tags.TagKey('key1')
-        val1 = tags.TagValue('value1')
+        key_1 = 'key1'
+        val1 = 'value1'
         tag = tags.Tag(key_1, val1)
         tag_map = tags.TagMap([tag])
 
         tag_map.update(key=key_1, value=val1)
         self.assertEqual({'key1': 'value1'}, tag_map.map)
 
-        key_2 = tags.TagKey('key2')
+        key_2 = 'key2'
         tag_map.update(key=key_2, value=val1)
         self.assertEqual({'key1': 'value1'}, tag_map.map)
 
-        val_2 = tags.TagValue('value2')
+        val_2 = 'value2'
         tag_map.update(key=key_1, value=val_2)
         self.assertEqual({'key1': 'value2'}, tag_map.map)
 
     def test_tag_key_exists(self):
-        key = tags.TagKey('key1')
-        value = tags.TagValue('val1')
+        key = 'key1'
+        value = 'val1'
         tag_map = tags.TagMap(tags=[tags.Tag(key, value)])
 
         self.assertTrue(tag_map.tag_key_exists(key))
