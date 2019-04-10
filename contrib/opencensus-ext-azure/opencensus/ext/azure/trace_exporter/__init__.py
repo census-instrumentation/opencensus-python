85e662161c6da1b9142145a1091240a3e2a03707
Reiley Yang <reyang@microsoft.com>
2019-04-10 15:42:49 -0700

    refactor

--- contrib/opencensus-ext-azure/opencensus/ext/azure/trace_exporter/__init__.py
+++ contrib/opencensus-ext-azure/opencensus/ext/azure/trace_exporter/__init__.py
@@ -26,76 +26,6 @@ from opencensus.trace import execution_context
 from opencensus.trace.span import SpanKind
 
 
-def span_data_to_envelope(sd):
-    print('[AzMon]', sd)
-    print('trace_id:', sd.context.trace_id)
-    print('tracestate:', sd.context.tracestate)
-    print('span_id:', sd.span_id)
-    print('parent_span_id:', sd.parent_span_id)
-    print('attributes:', sd.attributes)
-    print('start_time:', sd.start_time)
-    print('end_time:', sd.end_time)
-    envelope = Envelope(
-        iKey=self.config.instrumentation_key,
-        tags=dict(utils.azure_monitor_context),
-        time=sd.start_time,
-    )
-    envelope.tags['ai.operation.id'] = sd.context.trace_id
-    if sd.parent_span_id:
-        envelope.tags['ai.operation.parentId'] = '|{}.{}.'.format(
-            sd.context.trace_id,
-            sd.parent_span_id,
-        )
-    if sd.span_kind == SpanKind.SERVER:
-        envelope.name = 'Microsoft.ApplicationInsights.Request'
-        data = Request(
-            id='|{}.{}.'.format(sd.context.trace_id, sd.span_id),
-            duration=utils.timestamp_to_duration(
-                sd.start_time,
-                sd.end_time,
-            ),
-            responseCode='0',  # TODO
-            success=True,  # TODO
-        )
-        envelope.data = Data(baseData=data, baseType='RequestData')
-        if 'http.method' in sd.attributes:
-            data.name = sd.attributes['http.method']
-        if 'http.url' in sd.attributes:
-            data.name = data.name + ' ' + sd.attributes['http.url']
-        if 'http.status_code' in sd.attributes:
-            data.responseCode = sd.attributes['http.status_code']
-    else:
-        envelope.name = \
-            'Microsoft.ApplicationInsights.RemoteDependency'
-        data = RemoteDependency(
-            name=sd.name,  # TODO
-            id='|{}.{}.'.format(sd.context.trace_id, sd.span_id),
-            resultCode='0',  # TODO
-            duration=utils.timestamp_to_duration(
-                sd.start_time,
-                sd.end_time,
-            ),
-            success=True,  # TODO
-        )
-        envelope.data = Data(
-            baseData=data,
-            baseType='RemoteDependencyData',
-        )
-        if sd.span_kind == SpanKind.CLIENT:
-            data.type = 'HTTP'  # TODO
-            if 'http.url' in sd.attributes:
-                url = sd.attributes['http.url']
-                # TODO: error handling, probably put scheme as well
-                data.name = utils.url_to_dependency_name(url)
-            if 'http.status_code' in sd.attributes:
-                data.resultCode = sd.attributes['http.status_code']
-        else:
-            envelope.data.type = 'IN PROCESS'
-    # TODO: links, tracestate, tags, attrs
-    print(json.dumps(envelope))
-    return envelope
-
-
 class AzureExporter(base_exporter.Exporter):
     """An exporter that sends traces to Microsoft Azure Monitor.
 
@@ -114,6 +44,75 @@ class AzureExporter(base_exporter.Exporter):
         self.config = config or utils.Config()
         self.transport = transport(self)
 
+    def span_data_to_envelope(self, sd):
+        print('[AzMon]', sd)
+        print('trace_id:', sd.context.trace_id)
+        print('tracestate:', sd.context.tracestate)
+        print('span_id:', sd.span_id)
+        print('parent_span_id:', sd.parent_span_id)
+        print('attributes:', sd.attributes)
+        print('start_time:', sd.start_time)
+        print('end_time:', sd.end_time)
+        envelope = Envelope(
+            iKey=self.config.instrumentation_key,
+            tags=dict(utils.azure_monitor_context),
+            time=sd.start_time,
+        )
+        envelope.tags['ai.operation.id'] = sd.context.trace_id
+        if sd.parent_span_id:
+            envelope.tags['ai.operation.parentId'] = '|{}.{}.'.format(
+                sd.context.trace_id,
+                sd.parent_span_id,
+            )
+        if sd.span_kind == SpanKind.SERVER:
+            envelope.name = 'Microsoft.ApplicationInsights.Request'
+            data = Request(
+                id='|{}.{}.'.format(sd.context.trace_id, sd.span_id),
+                duration=utils.timestamp_to_duration(
+                    sd.start_time,
+                    sd.end_time,
+                ),
+                responseCode='0',  # TODO
+                success=True,  # TODO
+            )
+            envelope.data = Data(baseData=data, baseType='RequestData')
+            if 'http.method' in sd.attributes:
+                data.name = sd.attributes['http.method']
+            if 'http.url' in sd.attributes:
+                data.name = data.name + ' ' + sd.attributes['http.url']
+            if 'http.status_code' in sd.attributes:
+                data.responseCode = sd.attributes['http.status_code']
+        else:
+            envelope.name = \
+                'Microsoft.ApplicationInsights.RemoteDependency'
+            data = RemoteDependency(
+                name=sd.name,  # TODO
+                id='|{}.{}.'.format(sd.context.trace_id, sd.span_id),
+                resultCode='0',  # TODO
+                duration=utils.timestamp_to_duration(
+                    sd.start_time,
+                    sd.end_time,
+                ),
+                success=True,  # TODO
+            )
+            envelope.data = Data(
+                baseData=data,
+                baseType='RemoteDependencyData',
+            )
+            if sd.span_kind == SpanKind.CLIENT:
+                data.type = 'HTTP'  # TODO
+                if 'http.url' in sd.attributes:
+                    url = sd.attributes['http.url']
+                    # TODO: error handling, probably put scheme as well
+                    data.name = utils.url_to_dependency_name(url)
+                if 'http.status_code' in sd.attributes:
+                    data.resultCode = sd.attributes['http.status_code']
+            else:
+                envelope.data.type = 'IN PROCESS'
+        # TODO: links, tracestate, tags, attrs
+        print(json.dumps(envelope))
+        return envelope
+
     def emit(self, span_datas):
         """
         :type span_datas: list of :class:
@@ -123,7 +122,7 @@ class AzureExporter(base_exporter.Exporter):
         """
         envelopes = []
         for sd in span_datas:
-            envelope = span_data_to_envelope(sd)
+            envelope = self.span_data_to_envelope(sd)
             envelopes.append(envelope)
 
         # TODO: prevent requests being tracked
