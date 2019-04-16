00c65159ad36ae0ac1cab18a1b384f87c44d3575
Reiley Yang <reyang@microsoft.com>
2019-04-16 12:52:16 -0700

    decouple exporters from flask integration

--- contrib/opencensus-ext-flask/opencensus/ext/flask/flask_middleware.py
+++ contrib/opencensus-ext-flask/opencensus/ext/flask/flask_middleware.py
@@ -12,14 +12,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import inspect
 import logging
 import sys
 
 import flask
 from google.rpc import code_pb2
 
-from opencensus.common.transports import sync
+from opencensus.common import configuration
 from opencensus.trace import attributes_helper
 from opencensus.trace import execution_context
 from opencensus.trace import print_exporter
@@ -28,38 +27,20 @@ from opencensus.trace import stack_trace
 from opencensus.trace import status
 from opencensus.trace import tracer as tracer_module
 from opencensus.trace import utils
-from opencensus.trace.propagation import google_cloud_format
-from opencensus.trace.samplers import always_on, probability
+from opencensus.trace.propagation import trace_context_http_header_format
+from opencensus.trace.samplers import always_on
 
 HTTP_METHOD = attributes_helper.COMMON_ATTRIBUTES['HTTP_METHOD']
 HTTP_URL = attributes_helper.COMMON_ATTRIBUTES['HTTP_URL']
 HTTP_STATUS_CODE = attributes_helper.COMMON_ATTRIBUTES['HTTP_STATUS_CODE']
 
 BLACKLIST_PATHS = 'BLACKLIST_PATHS'
-GCP_EXPORTER_PROJECT = 'GCP_EXPORTER_PROJECT'
-SAMPLING_RATE = 'SAMPLING_RATE'
-TRANSPORT = 'TRANSPORT'
-SERVICE_NAME = 'SERVICE_NAME'
-ZIPKIN_EXPORTER_SERVICE_NAME = 'ZIPKIN_EXPORTER_SERVICE_NAME'
-ZIPKIN_EXPORTER_HOST_NAME = 'ZIPKIN_EXPORTER_HOST_NAME'
-ZIPKIN_EXPORTER_PORT = 'ZIPKIN_EXPORTER_PORT'
-ZIPKIN_EXPORTER_PROTOCOL = 'ZIPKIN_EXPORTER_PROTOCOL'
-JAEGER_EXPORTER_HOST_NAME = 'JAEGER_EXPORTER_HOST_NAME'
-JAEGER_EXPORTER_PORT = 'JAEGER_EXPORTER_PORT'
-JAEGER_EXPORTER_AGENT_HOST_NAME = 'JAEGER_EXPORTER_AGENT_HOST_NAME'
-JAEGER_EXPORTER_AGENT_PORT = 'JAEGER_EXPORTER_AGENT_PORT'
-JAEGER_EXPORTER_SERVICE_NAME = 'JAEGER_EXPORTER_SERVICE_NAME'
-OCAGENT_TRACE_EXPORTER_ENDPOINT = 'OCAGENT_TRACE_EXPORTER_ENDPOINT'
 BLACKLIST_HOSTNAMES = 'BLACKLIST_HOSTNAMES'
 
 log = logging.getLogger(__name__)
 
 
 class FlaskMiddleware(object):
-    DEFAULT_SAMPLER = always_on.AlwaysOnSampler
-    DEFAULT_EXPORTER = print_exporter.PrintExporter
-    DEFAULT_PROPAGATOR = google_cloud_format.GoogleCloudFormatPropagator
-
     """Flask middleware to automatically trace requests.
 
     :type app: :class: `~flask.Flask`
@@ -68,24 +49,25 @@ class FlaskMiddleware(object):
     :type blacklist_paths: list
     :param blacklist_paths: Paths that do not trace.
 
-    :type sampler: :class: `type`
-    :param sampler: Class for creating new Sampler objects. It should extend
+    :type sampler: :class:`~opencensus.trace.samplers.base.Sampler`
+    :param sampler: Instance of sampler objects. It should extend
                     from the base :class:`.Sampler` type and implement
                     :meth:`.Sampler.should_sample`. Defaults to
                     :class:`.AlwaysOnSampler`. The rest options are
                     :class:`.AlwaysOffSampler`, :class:`.FixedRateSampler`.
 
-    :type exporter: :class: `type`
-    :param exporter: Class for creating new exporter objects. Default to
-                     :class:`.PrintExporter`. The rest option is
-                     :class:`.FileExporter`.
+    :type exporter: :class:`~opencensus.trace.base_exporter.exporter`
+    :param exporter: Instance of exporter objects. Default to
+                     :class:`.PrintExporter`. The rest options are
+                     :class:`.FileExporter`, :class:`.LoggingExporter` and
+                     trace exporter extensions.
 
-    :type propagator: :class: 'type'
-    :param propagator: Class for creating new propagator objects. Default to
-                       :class:`.GoogleCloudFormatPropagator`. The rest option
+    :type propagator: :class: 'object'
+    :param propagator: Instance of propagator objects. Default to
+                       :class:`.TraceContextPropagator`. The rest options
                        are :class:`.BinaryFormatPropagator`,
-                       :class:`.TextFormatPropagator` and
-                       :class:`.TraceContextPropagator`.
+                       :class:`.GoogleCloudFormatPropagator` and
+                       :class:`.TextFormatPropagator`.
     """
 
     def __init__(self, app=None, blacklist_paths=None, sampler=None,
@@ -105,90 +87,28 @@ class FlaskMiddleware(object):
         # get settings from app config
         settings = self.app.config.get('OPENCENSUS_TRACE', {})
 
-        self.sampler = (self.sampler
-                        or settings.get('SAMPLER',
-                                        self.DEFAULT_SAMPLER))
-        self.exporter = (self.exporter
-                         or settings.get('EXPORTER',
-                                         self.DEFAULT_EXPORTER))
-        self.propagator = (self.propagator
-                           or settings.get('PROPAGATOR',
-                                           self.DEFAULT_PROPAGATOR))
+        if self.sampler is None:
+            self.sampler = settings.get('SAMPLER', None) or \
+                always_on.AlwaysOnSampler()
+            if isinstance(self.sampler, str):
+                self.sampler = configuration.load(self.sampler)
 
-        # get params from app config
-        params = self.app.config.get('OPENCENSUS_TRACE_PARAMS', {})
+        if self.exporter is None:
+            self.exporter = settings.get('EXPORTER', None) or \
+                print_exporter.PrintExporter()
+            if isinstance(self.exporter, str):
+                self.exporter = configuration.load(self.exporter)
 
-        self.blacklist_paths = params.get(BLACKLIST_PATHS,
-                                          self.blacklist_paths)
+        if self.propagator is None:
+            self.propagator = settings.get('PROPAGATOR', None) or \
+                trace_context_http_header_format.TraceContextPropagator()
+            if isinstance(self.propagator, str):
+                self.propagator = configuration.load(self.propagator)
 
-        # Initialize the sampler
-        if not inspect.isclass(self.sampler):
-            pass  # handling of instantiated sampler
-        elif self.sampler.__name__ == 'ProbabilitySampler':
-            _rate = params.get(SAMPLING_RATE,
-                               probability.DEFAULT_SAMPLING_RATE)
-            self.sampler = self.sampler(_rate)
-        else:
-            self.sampler = self.sampler()
+        self.blacklist_paths = settings.get(BLACKLIST_PATHS,
+                                            self.blacklist_paths)
 
-        transport = params.get(TRANSPORT, sync.SyncTransport)
-
-        # Initialize the exporter
-        if not inspect.isclass(self.exporter):
-            pass  # handling of instantiated exporter
-        elif self.exporter.__name__ == 'StackdriverExporter':
-            _project_id = params.get(GCP_EXPORTER_PROJECT, None)
-            self.exporter = self.exporter(
-                project_id=_project_id,
-                transport=transport)
-        elif self.exporter.__name__ == 'JaegerExporter':
-            _service_name = params.get(
-                JAEGER_EXPORTER_SERVICE_NAME,
-                self._get_service_name(params))
-            _jaeger_host_name = params.get(
-                JAEGER_EXPORTER_HOST_NAME, None)
-            _jaeger_port = params.get(
-                JAEGER_EXPORTER_PORT, None)
-            _jaeger_agent_host_name = params.get(
-                JAEGER_EXPORTER_AGENT_HOST_NAME, 'localhost')
-            _jaeger_agent_port = params.get(
-                JAEGER_EXPORTER_AGENT_PORT, 6831)
-            self.exporter = self.exporter(
-                service_name=_service_name,
-                host_name=_jaeger_host_name,
-                port=_jaeger_port,
-                agent_host_name=_jaeger_agent_host_name,
-                agent_port=_jaeger_agent_port,
-                transport=transport)
-        elif self.exporter.__name__ == 'ZipkinExporter':
-            _service_name = self._get_service_name(params)
-            _zipkin_host_name = params.get(
-                ZIPKIN_EXPORTER_HOST_NAME, 'localhost')
-            _zipkin_port = params.get(
-                ZIPKIN_EXPORTER_PORT, 9411)
-            _zipkin_protocol = params.get(
-                ZIPKIN_EXPORTER_PROTOCOL, 'http')
-            self.exporter = self.exporter(
-                service_name=_service_name,
-                host_name=_zipkin_host_name,
-                port=_zipkin_port,
-                protocol=_zipkin_protocol,
-                transport=transport)
-        elif self.exporter.__name__ == 'TraceExporter':
-            _service_name = self._get_service_name(params)
-            _endpoint = params.get(
-                OCAGENT_TRACE_EXPORTER_ENDPOINT, None)
-            self.exporter = self.exporter(
-                service_name=_service_name,
-                endpoint=_endpoint,
-                transport=transport)
-        else:
-            self.exporter = self.exporter(transport=transport)
-
-        self.blacklist_hostnames = params.get(BLACKLIST_HOSTNAMES, None)
-        # Initialize the propagator
-        if inspect.isclass(self.propagator):
-            self.propagator = self.propagator()
+        self.blacklist_hostnames = settings.get(BLACKLIST_HOSTNAMES, None)
 
         self.setup_trace()
 
@@ -280,13 +200,3 @@ class FlaskMiddleware(object):
             tracer.finish()
         except Exception:  # pragma: NO COVER
             log.error('Failed to trace request', exc_info=True)
-
-    def _get_service_name(self, params):
-        _service_name = params.get(
-            SERVICE_NAME, None)
-
-        if _service_name is None:
-            _service_name = params.get(
-                ZIPKIN_EXPORTER_SERVICE_NAME, 'my_service')
-
-        return _service_name
