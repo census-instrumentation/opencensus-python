585c73b78511497885f5d52bb170368955514268
Leighton <lechen@microsoft.com>
2019-06-26 16:18:41 -0700

    Add space

--- contrib/opencensus-ext-azure/opencensus/ext/azure/common/storage.py
+++ contrib/opencensus-ext-azure/opencensus/ext/azure/common/storage.py
@@ -188,7 +188,7 @@ class LocalNoopStorage(object):
         self.write_timeout = write_timeout
 
     def gets(self):
-        return [] # pragma: no cover
+        return []  # pragma: no cover
 
     def put(self, data, lease_period=0, silent=False):
-        return # pragma: no cover
+        return  # pragma: no cover
