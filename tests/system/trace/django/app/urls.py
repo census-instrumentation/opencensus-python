095cdb6e0fa91ccc8e508a1c05e86f4ed4a45f68
Tim Graham <timograham@gmail.com>
2019-06-26 22:36:37 -0400

    Remove support for Django < 1.11 (#694)

--- tests/system/trace/django/app/urls.py
+++ tests/system/trace/django/app/urls.py
@@ -27,14 +27,14 @@ Including another URLconf
     1. Add an import:  from blog import urls as blog_urls
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import include, url
+from django.conf.urls import url
 from django.contrib import admin
 
 import app.views
 
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
     url(r'^$', app.views.home),
     url(r'^greetings$', app.views.greetings),
     url(r'^_ah/health$', app.views.health_check),
