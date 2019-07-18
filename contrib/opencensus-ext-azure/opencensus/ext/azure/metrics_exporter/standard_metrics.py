75c27616901f767b57ee06697c73a414c4a8130f
Leighton <lechen@microsoft.com>
2019-07-18 14:57:53 -0700

    Refactor standard metrics to files

--- contrib/opencensus-ext-azure/opencensus/ext/azure/metrics_exporter/standard_metrics.py
+++ contrib/opencensus-ext-azure/opencensus/ext/azure/metrics_exporter/standard_metrics.py
@@ -12,131 +12,22 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import logging
-import psutil
-
-from opencensus.metrics.export.gauge import DerivedDoubleGauge
-from opencensus.metrics.export.gauge import DerivedLongGauge
 from opencensus.metrics.export.gauge import Registry
 from opencensus.metrics.export.metric_producer import MetricProducer
+from opencensus.ext.azure.metrics_exporter.available_memory import AvailableMemoryMetric
+from opencensus.ext.azure.metrics_exporter.cpu_usage import ProcessorTimeMetric
+from opencensus.ext.azure.metrics_exporter.process import ProcessCPUMetric
+from opencensus.ext.azure.metrics_exporter.process import ProcessMemoryMetric
 
-logger = logging.getLogger(__name__)
-PROCESS = psutil.Process()
-
-# Namespaces used in Azure Monitor
-AVAILABLE_MEMORY = "\\Memory\\Available Bytes"
-PRIVATE_BYTES = "\\Process(??APP_WIN32_PROC??)\\Private Bytes"
-PROCESSOR_TIME = "\\Processor(_Total)\\% Processor Time"
-PROCESS_TIME = "\\Process(??APP_WIN32_PROC??)\\% Processor Time"
+STANDARD_METRICS = [AvailableMemoryMetric, ProcessCPUMetric, ProcessMemoryMetric, ProcessorTimeMetric]
 
 
-def get_available_memory():
-    return psutil.virtual_memory().available
-
-
-def get_available_memory_metric():
-    """ Returns a derived gauge for available memory
-
-    Available memory is defined as memory that can be given instantly to
-    processes without the system going into swap.
-
-    :rtype: :class:`opencensus.metrics.export.gauge.DerivedLongGauge`
-    :return: The gauge representing the available memory metric
-    """
-    gauge = DerivedLongGauge(
-        AVAILABLE_MEMORY,
-        'Amount of available memory in bytes',
-        'byte',
-        [])
-    gauge.create_default_time_series(get_available_memory)
-    return gauge
-
-
-def get_process_private_bytes():
-    try:
-        return PROCESS.memory_info().rss
-    except Exception:
-        logger.exception('Error handling get process private bytes.')
-
-
-def get_process_private_bytes_metric():
-    """ Returns a derived gauge for private bytes for the current process
-
-    Private bytes for the current process is measured by the Resident Set
-    Size, which is the non-swapped physical memory a process has used.
-
-    :rtype: :class:`opencensus.metrics.export.gauge.DerivedLongGauge`
-    :return: The gauge representing the private bytes metric
-    """
-    gauge = DerivedLongGauge(
-        PRIVATE_BYTES,
-        'Amount of memory process has used in bytes',
-        'byte',
-        [])
-    gauge.create_default_time_series(get_process_private_bytes)
-    return gauge
-
-
-def get_processor_time():
-    cpu_times_percent = psutil.cpu_times_percent()
-    return 100 - cpu_times_percent.idle
-
-
-def get_processor_time_metric():
-    """ Returns a derived gauge for the processor time.
-
-    Processor time is defined as a float representing the current system
-    wide CPU utilization minus idle CPU time as a percentage. Idle CPU
-    time is defined as the time spent doing nothing. Return values range
-    from 0.0 to 100.0 inclusive.
-
-    :rtype: :class:`opencensus.metrics.export.gauge.DerivedDoubleGauge`
-    :return: The gauge representing the processor time metric
-    """
-    gauge = DerivedDoubleGauge(
-        PROCESSOR_TIME,
-        'Processor time as a percentage',
-        'percentage',
-        [])
-    gauge.create_default_time_series(get_processor_time)
-    # From the psutil docs: the first time this method is called with interval
-    # = None it will return a meaningless 0.0 value which you are supposed to
-    # ignore. Call cpu_percent() once so that the subsequent calls from the
-    # gauge will be meaningful.
-    psutil.cpu_times_percent()
-    return gauge
-
-
-def get_process_cpu_usage():
-    try:
-        # In the case of a process running on multiple threads on different CPU
-        # cores, the returned value of cpu_percent() can be > 100.0. The actual
-        # value that is return from this function should be capped at 100.
-        return min(PROCESS.cpu_percent(), 100.0)
-    except Exception:
-        logger.exception('Error handling get process cpu usage.')
-
-
-def get_process_cpu_usage_metric():
-    """ Returns a derived gauge for the CPU usage for the current process.
-
-    Return values range from 0.0 to 100.0 inclusive.
-
-    :rtype: :class:`opencensus.metrics.export.gauge.DerivedDoubleGauge`
-    :return: The gauge representing the process cpu usage metric
-    """
-    gauge = DerivedDoubleGauge(
-        PROCESS_TIME,
-        'Processor time as a percentage',
-        'percentage',
-        [])
-    gauge.create_default_time_series(get_process_cpu_usage)
-    # From the psutil docs: the first time this method is called with interval
-    # = None it will return a meaningless 0.0 value which you are supposed to
-    # ignore. Call cpu_percent() with process once so that the subsequent calls
-    # from the gauge will be meaningful.
-    PROCESS.cpu_percent()
-    return gauge
+def register_metrics():
+    registry = Registry()
+    for standard_metric in STANDARD_METRICS:
+        metric = standard_metric()
+        registry.add_gauge(metric())
+    return registry
 
 
 class AzureStandardMetricsProducer(MetricProducer):
@@ -146,11 +37,7 @@ class AzureStandardMetricsProducer(MetricProducer):
     using gauges.
     """
     def __init__(self):
-        self.registry = Registry()
-        self.registry.add_gauge(get_available_memory_metric())
-        self.registry.add_gauge(get_process_private_bytes_metric())
-        self.registry.add_gauge(get_processor_time_metric())
-        self.registry.add_gauge(get_process_cpu_usage_metric())
+        self.registry = register_metrics()
 
     def get_metrics(self):
         return self.registry.get_metrics()
