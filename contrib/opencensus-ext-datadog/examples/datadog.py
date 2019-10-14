d7d31eba33f539c2367ca6389d995801c9c9c4ab
Nathan Button <nbutton23@users.noreply.github.com>
2019-10-14 09:08:50 -0700

    Create README.rst for the Datadog trace exporter (#799)

--- contrib/opencensus-ext-datadog/examples/datadog.py
+++ contrib/opencensus-ext-datadog/examples/datadog.py
@@ -1,8 +1,8 @@
 from flask import Flask
 
+from opencensus.ext.datadog.traces import DatadogTraceExporter, Options
 from opencensus.ext.flask.flask_middleware import FlaskMiddleware
 from opencensus.trace.samplers import AlwaysOnSampler
-from traces import DatadogTraceExporter, Options
 
 app = Flask(__name__)
 middleware = FlaskMiddleware(app,
