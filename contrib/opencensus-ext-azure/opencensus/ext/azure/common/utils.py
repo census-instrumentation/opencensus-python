2b9ce4069e954ed10acce3763f42d3b97d73d137
Leighton <lechen@microsoft.com>
2019-10-23 13:56:25 -0700

    fix lint

--- contrib/opencensus-ext-azure/opencensus/ext/azure/common/utils.py
+++ contrib/opencensus-ext-azure/opencensus/ext/azure/common/utils.py
@@ -71,10 +71,10 @@ def url_to_dependency_name(url):
 # Validate UUID format
 # Specs taken from https://tools.ietf.org/html/rfc4122
 uuid_regex_pattern = re.compile('^[0-9a-f]{8}-'
-                     '[0-9a-f]{4}-'
-                     '[1-5][0-9a-f]{3}-'
-                     '[89ab][0-9a-f]{3}-'
-                     '[0-9a-f]{12}$')
+                                '[0-9a-f]{4}-'
+                                '[1-5][0-9a-f]{3}-'
+                                '[89ab][0-9a-f]{3}-'
+                                '[0-9a-f]{12}$')
 
 
 def validate_instrumentation_key(instrumentation_key):
