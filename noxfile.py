33596219e41f7afac199c215e67b0b5e08022110
Leighton <lechen@microsoft.com>
2020-01-13 15:26:38 -0800

    comment

--- noxfile.py
+++ noxfile.py
@@ -25,7 +25,7 @@ def _install_dev_packages(session):
     session.install('-e', '.')
 
     session.install('-e', 'contrib/opencensus-ext-azure')
-    session.install('-e', 'contrib/opencensus-ext-datadog')
+    # session.install('-e', 'contrib/opencensus-ext-datadog')
     session.install('-e', 'contrib/opencensus-ext-dbapi')
     session.install('-e', 'contrib/opencensus-ext-django')
     session.install('-e', 'contrib/opencensus-ext-flask')
