d680561d3124650af371aec4be699f3c62cbd7cf
Chris Kleinknecht <libc@google.com>
2020-01-14 16:35:02 -0800

    CircleCI check for sqlite with deps

--- noxfile.py
+++ noxfile.py
@@ -87,6 +87,8 @@ def unit(session):
 
 @nox.session(python=['2.7', '3.4', '3.5', '3.6'])
 def sqlite_check(session):
+    _install_test_dependencies(session)
+    _install_dev_packages(session)
     session.run('python', '-c', 'import sqlite3')
 
 
