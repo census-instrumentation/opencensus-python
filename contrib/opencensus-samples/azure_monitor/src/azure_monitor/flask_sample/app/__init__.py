be221add3aaa9039d229c5704a76488ee1fd64d4
Leighton <lechen@microsoft.com>
2020-02-14 15:30:11 -0800

    lint

--- contrib/opencensus-samples/azure_monitor/src/azure_monitor/flask_sample/app/__init__.py
+++ contrib/opencensus-samples/azure_monitor/src/azure_monitor/flask_sample/app/__init__.py
@@ -15,9 +15,9 @@
 import logging
 
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
 from opencensus.ext.azure import metrics_exporter
 from opencensus.ext.azure.log_exporter import AzureLogHandler
 from opencensus.ext.flask.flask_middleware import FlaskMiddleware
