622dc7d8556448b57ae2571e31c6f6977df5ba88
Anam Navied <anavied@ncsu.edu>
2020-04-30 16:07:22 -0400

    fixed rpc_m_c_test.py pylint errors

--- opencensus/grpc/rpc_m_c_test.py
+++ opencensus/grpc/rpc_m_c_test.py
@@ -1,5 +1,4 @@
 import unittest
-# import rpc_measure_constants
 from opencensus.grpc import rpc_measure_constants
 
 
@@ -56,38 +55,58 @@ class RPCMCTest(unittest.TestCase):
         """
         tests the server measures
         """
-        self.assertEqual(self.rpc_measure.grpc_server_received_messages_per_rpc.name,
-                         "grpc.io/server/received_messages_per_rpc",
-                         "grpc_server_received_messages_per_rpc not set properly")
-        self.assertEqual(self.rpc_measure.grpc_server_received_bytes_per_rpc.description,
-                         "Total bytes received across all messages per RPC",
-                         "grpc_server_received_bytes_per_rpc not set properly")
-        self.assertEqual(self.rpc_measure.grpc_server_sent_messages_per_rpc.unit,
-                         "1",
-                         "grpc_server_sent_messages_per_rpc not set properly")
-        self.assertEqual(self.rpc_measure.grpc_server_sent_bytes_per_rpc.name,
-                         "grpc.io/server/sent_bytes_per_rpc",
-                         "grpc_server_sent_bytes_per_rpc not set properly")
-        self.assertEqual(self.rpc_measure.grpc_server_server_latency.description,
-                         "Time between first byte of request received to"
-                         " last byte of response sent or terminal error.",
-                         "grpc_server_server_latency not set properly")
-        self.assertEqual(self.rpc_measure.grpc_server_started_rpcs.unit,
-                         "1",
-                         "grpc_server_started_rpcs not set correctly")
-        self.assertEqual(self.rpc_measure.grpc_server_sent_messages_per_method.name,
-                         "grpc.io/server/sent_messages_per_method",
-                         "grpc_server_sent_messages_per_method not set correctly")
-        self.assertEqual(self.rpc_measure.grpc_server_received_messages_per_method.description,
-                         "Total messages received per method.",
-                         "grpc_server_received_messages_per_method not set correctly")
-        self.assertEqual(self.rpc_measure.grpc_server_sent_bytes_per_method.unit,
-                         "by",
-                         "grpc_server_sent_bytes_per_method not set correctly")
-        self.assertEqual(self.rpc_measure.grpc_server_received_bytes_per_method.name,
-                         "grpc.io/server/received_bytes_per_method",
-                         "grpc_server_received_bytes_per_method not set correctly")
+        self.assertEqual(
+            self.rpc_measure.grpc_server_received_messages_per_rpc.name,
+            "grpc.io/server/received_messages_per_rpc",
+            "grpc_server_received_messages_per_rpc not set properly")
 
+        self.assertEqual(
+            self.rpc_measure.grpc_server_received_bytes_per_rpc.description,
+            "Total bytes received across all messages per RPC",
+            "grpc_server_received_bytes_per_rpc not set properly")
+
+        self.assertEqual(
+            self.rpc_measure.grpc_server_sent_messages_per_rpc.unit,
+            "1",
+            "grpc_server_sent_messages_per_rpc not set properly")
+
+        self.assertEqual(
+            self.rpc_measure.grpc_server_sent_bytes_per_rpc.name,
+            "grpc.io/server/sent_bytes_per_rpc",
+            "grpc_server_sent_bytes_per_rpc not set properly")
+
+        self.assertEqual(
+            self.rpc_measure.grpc_server_server_latency.description,
+            "Time between first byte of request received to"
+            " last byte of response sent or terminal error.",
+            "grpc_server_server_latency not set properly")
+
+        self.assertEqual(
+            self.rpc_measure.grpc_server_started_rpcs.unit,
+            "1",
+            "grpc_server_started_rpcs not set correctly")
+
+        self.assertEqual(
+            self.rpc_measure.grpc_server_sent_messages_per_method.name,
+            "grpc.io/server/sent_messages_per_method",
+            "grpc_server_sent_messages_per_method not set correctly")
+
+        self.assertEqual(
+            self.rpc_measure.grpc_server_received_messages_per_method.
+            description,
+            "Total messages received per method.",
+            "grpc_server_received_messages_per_method not set correctly")
+
+        self.assertEqual(
+            self.rpc_measure.grpc_server_sent_bytes_per_method.unit,
+            "by",
+            "grpc_server_sent_bytes_per_method not set correctly")
+
+        self.assertEqual(
+            self.rpc_measure.grpc_server_received_bytes_per_method.name,
+            "grpc.io/server/received_bytes_per_method",
+            "grpc_server_received_bytes_per_method not set correctly")
+TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT
 
 if __name__ == '__main__':
     unittest.main()
