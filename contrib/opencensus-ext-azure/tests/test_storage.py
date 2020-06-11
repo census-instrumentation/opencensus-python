e0e6361b7065294bb04aa8b321cee61394ac4d45
Leighton <lechen@microsoft.com>
2020-06-11 15:14:00 -0700

    lint

--- contrib/opencensus-ext-azure/tests/test_storage.py
+++ contrib/opencensus-ext-azure/tests/test_storage.py
@@ -170,7 +170,8 @@ class TestLocalFileStorage(unittest.TestCase):
                 stor._maintenance_routine()
                 stor._maintenance_routine()
                 self.assertEqual(p.call_count, 2)
-            with mock.patch('os.path.isdir', side_effect=throw(Exception)) as isdir:
+            with mock.patch(
+                'os.path.isdir', side_effect=throw(Exception)) as isdir:
                 stor._maintenance_routine()
                 stor._maintenance_routine()
                 self.assertEqual(isdir.call_count, 2)
