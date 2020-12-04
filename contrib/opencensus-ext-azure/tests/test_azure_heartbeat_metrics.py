be9ee31812f7c7b781c915b509de0e9c39070636
Aaron Abbott <aaronabbott@google.com>
2020-12-04 23:01:35 +0000

    mock out azure metadata server, which exists in GHA environment

--- contrib/opencensus-ext-azure/tests/test_azure_heartbeat_metrics.py
+++ contrib/opencensus-ext-azure/tests/test_azure_heartbeat_metrics.py
@@ -89,7 +89,10 @@ class TestHeartbeatMetrics(unittest.TestCase):
         self.assertFalse(metric.init)
         self.assertEqual(len(metric.properties), 0)
 
-    # @unittest.skip("L101 is failing in Github action run for some reason")
+    @mock.patch(
+        'requests.get',
+        throw(requests.exceptions.ConnectionError)
+    )
     def test_heartbeat_metric_get_metric_init(self):
         metric = heartbeat_metrics.HeartbeatMetric()
         self.assertFalse(metric.init)
@@ -98,7 +101,6 @@ class TestHeartbeatMetrics(unittest.TestCase):
         self.assertEqual(metric.NAME, 'Heartbeat')
         keys = list(metric.properties.keys())
         values = list(metric.properties.values())
-        self.fail("WEBSITE_SITE_NAME value is '{}' \nproperty keys: {}".format(os.environ.get("WEBSITE_SITE_NAME"), keys))
         self.assertEqual(len(keys), 2)
         self.assertEqual(len(keys), len(values))
         self.assertEqual(keys[0].key, "sdk")
