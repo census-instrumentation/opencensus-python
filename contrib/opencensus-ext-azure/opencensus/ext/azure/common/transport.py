03f00d3a07433d71d665ab90a2a250e7c10a38e0
Leighton Chen <lechen@microsoft.com>
2021-04-08 11:27:15 -0700

    lint

--- contrib/opencensus-ext-azure/opencensus/ext/azure/common/transport.py
+++ contrib/opencensus-ext-azure/opencensus/ext/azure/common/transport.py
@@ -16,12 +16,11 @@ import json
 import logging
 
 import requests
-
 from azure.core.exceptions import ClientAuthenticationError
 from azure.identity._exceptions import CredentialUnavailableError
 
 logger = logging.getLogger(__name__)
-_MONITOR_OAUTH_SCOPE = "https://monitor.azure.com/.default"
+_MONITOR_OAUTH_SCOPE = "https://monitor.azure.com//.default"
 
 class TransportMixin(object):
     def _transmit_from_storage(self):
