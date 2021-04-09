b7534c27545277bea3b9e57871d9f18af546d06d
Leighton Chen <lechen@microsoft.com>
2021-04-08 17:13:40 -0700

    gevent

---
+++ contrib/opencensus-ext-gevent/CHANGELOG.md
@@ -0,0 +1,8 @@
+# Changelog
+
+## Unreleased
+
+## 0.1.0
+Released 2019-05-31
+
+- Initial version
---
+++ contrib/opencensus-ext-gevent/setup.py
@@ -0,0 +1,56 @@
+# Copyright 2019, OpenCensus Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from setuptools import find_packages, setup
+
+from version import __version__
+
+setup(
+    name='opencensus-ext-gevent',
+    version=__version__,  # noqa
+    author='OpenCensus Authors',
+    author_email='census-developers@googlegroups.com',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    description='OpenCensus gevent compatibility helper',
+    include_package_data=True,
+    long_description=open('README.rst').read(),
+    install_requires=[
+        'opencensus >= 0.8.dev0, < 1.0.0',
+        'gevent >= 1.3'
+    ],
+    extras_require={},
+    license='Apache-2.0',
+    packages=find_packages(exclude=('tests',)),
+    namespace_packages=[],
+    url='https://github.com/census-instrumentation/opencensus-python/tree/master/contrib/opencensus-ext-gevent',  # noqa: E501
+    zip_safe=False,
+    entry_points={
+        "gevent.plugins.monkey.did_patch_builtins": [
+            "opencensus = opencensus.ext.gevent.geventcompatibility:patch_opencensus"  # noqa: E501
+        ]
+    },
+)
