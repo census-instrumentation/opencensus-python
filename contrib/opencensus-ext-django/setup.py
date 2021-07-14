c2d8de91cb2143c10f9b5408236f5e41f24b95cf
Aaron Abbott <aaronabbott@google.com>
2021-07-14 10:47:29 -0400

    Add newer python 3 versions (#1048)

--- contrib/opencensus-ext-django/setup.py
+++ contrib/opencensus-ext-django/setup.py
@@ -38,6 +38,8 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     description='OpenCensus Django Integration',
     include_package_data=True,
