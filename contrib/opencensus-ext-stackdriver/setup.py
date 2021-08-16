0ea194588f2c22a7a2baf80a48b9094a6a3f9c4a
Aaron Abbott <aaronabbott@google.com>
2021-08-16 15:31:46 +0000

    Release opencensus-ext-stackdriver v0.8.0 (post release)

--- contrib/opencensus-ext-stackdriver/setup.py
+++ contrib/opencensus-ext-stackdriver/setup.py
@@ -40,7 +40,7 @@ setup(
         'google-cloud-monitoring ~= 2.0',
         'google-cloud-trace >= 0.20.0, < 1.0.0',
         'rsa <= 4.0; python_version<="3.4"',
-        'opencensus >= 0.7.13, < 1.0.0',
+        'opencensus >= 0.8.dev0, < 1.0.0',
     ],
     python_requires=">=3.6",
     extras_require={},
