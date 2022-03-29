9ffa48a7dbb7f3fc68bc6cd60ab8d271d08f2c6a
Konstantin Gukov <gukkos@gmail.com>
2022-03-29 23:36:39 +0200

    Make sure handler.flush() doesn't deadlock. (#1112)

--- contrib/opencensus-ext-azure/opencensus/ext/azure/log_exporter/__init__.py
+++ contrib/opencensus-ext-azure/opencensus/ext/azure/log_exporter/__init__.py
@@ -106,6 +106,20 @@ class BaseLogHandler(logging.Handler):
         raise NotImplementedError  # pragma: NO COVER
 
     def flush(self, timeout=None):
+        if self._queue.is_empty():
+            return
+
+        # We must check the worker thread is alive, because otherwise flush
+        # is useless. Also, it would deadlock if no timeout is given, and the
+        # queue isn't empty.
+        # This is a very possible scenario during process termination, when
+        # atexit first calls handler.close() and then logging.shutdown(),
+        # that in turn calls handler.flush() without arguments.
+        if not self._worker.is_alive():
+            logger.warning("Can't flush %s, worker thread is dead. "
+                           "Any pending messages will be lost.", self)
+            return
+
         self._queue.flush(timeout=timeout)
 
 
