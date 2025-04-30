f068b92211774794efd4f1e204058f0b141e3e44
Daniel Garcia Moreno <daniel.garcia@suse.com>
2025-04-30 09:44:28 +0200

    Set DummyLock to BaseLogHandler
    This patch adds a DummyLock to do not synchronising handling, as
    suggested in the comment:
    https://github.com/census-instrumentation/opencensus-python/pull/1248#issuecomment-2840942763

--- contrib/opencensus-ext-azure/opencensus/ext/azure/log_exporter/__init__.py
+++ contrib/opencensus-ext-azure/opencensus/ext/azure/log_exporter/__init__.py
@@ -41,6 +41,27 @@ logger = logging.getLogger(__name__)
 __all__ = ['AzureEventHandler', 'AzureLogHandler']
 
 
+class DummyLock:
+    """
+    Implements the threading.Lock interface and do nothing
+    """
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        return self
+
+    def acquire(self, *args, **kwargs):
+        pass
+
+    def release(self):
+        pass
+
+    def _at_fork_reinit(self):
+        pass
+
+
 class BaseLogHandler(logging.Handler):
 
     def __init__(self, **options):
@@ -101,6 +122,10 @@ class BaseLogHandler(logging.Handler):
         if hasattr(self, "_worker") and self._worker:
             self._worker.stop(timeout)
         super(BaseLogHandler, self).close()
+
+    def createLock(self):
+        # not synchronising handling
+        self.lock = DummyLock()
 
     def emit(self, record):
         self._queue.put(record, block=False)
